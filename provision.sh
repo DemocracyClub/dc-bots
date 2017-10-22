2eca7c64b197efd820e5137977923dae740fc144
chris48s <chris.shaw480@gmail.com>
2017-10-22 14:38:00 +0100

    fail on error, ensure no user interaction

--- provision.sh
+++ provision.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-apt-get update && apt-get upgrade
+set -e
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get -y update && apt-get -y upgrade
 apt-get install -y git-core bash-completion git
 apt-get install -y jq
-DEBIAN_FRONTEND=noninteractive apt-get install -y mailutils
+apt-get install -y mailutils
 apt-get install -y python-pip
 pip install virtualenvwrapper
 source /usr/local/bin/virtualenvwrapper.sh
